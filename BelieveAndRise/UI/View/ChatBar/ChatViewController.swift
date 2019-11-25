86a799e70130a329b24ca50317a457d0fc15666b
MasterBel2 <39206414+MasterBel2@users.noreply.github.com>
2019-11-25 11:25:34 +1000

    Move `ChatController` to its own file.
    Was in the same file as `ChatViewController`

--- BelieveAndRise/UI/View/ChatBar/ChatViewController.swift
+++ BelieveAndRise/UI/View/ChatBar/ChatViewController.swift
@@ -8,61 +8,6 @@
 
 import Cocoa
 
-final class ChatController {
-
-    let server: TASServer
-    let windowManager: WindowManager
-
-    var channels: [ChannelSummary] = []
-
-    struct ChannelSummary {
-        let title: String
-        let description: String
-        let members: String
-        let isPrivate: Bool
-    }
-
-    init(server: TASServer, windowManager: WindowManager) {
-        self.server = server
-        self.windowManager = windowManager
-    }
-
-    func ring(_ id: Int) {}
-
-    func sendMessage(_ message: String, toChannelNamed channelName: String) {
-        server.send(CSSayCommand(channelName: channelName, message: message))
-    }
-
-    func sendPrivateMessage(_ message: String, toUserIdentifiedBy id: Int) {
-
-    }
-
-    func ignoreUser(_ id: Int) {
-        // 1. Present a prompt asking for a reason ????
-
-        // 2. Allow discarding of message and cancelling of the ignore action on
-
-        // 3. Send message on prompt completion
-    }
-
-    func unignoreUser(_ id: Int) {
-
-    }
-
-    func presentJoinChannelMenu() {
-        //
-    }
-
-    func joinChannel(_ channelName: String) {
-        if channels.first(where: { $0.title == channelName })?.isPrivate == true {
-            // Present a prompt
-        }
-        server.send(CSJoinCommand(channelName: channelName, key: nil))
-    }
-}
-
-
-
 final class ChatViewController: NSViewController, ChatBarControllerDelegate {
 
     // MARK: - User Interface
