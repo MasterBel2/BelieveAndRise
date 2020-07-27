ff4da6c19130cca41be83e21859561ce1d17836d
MasterBel2 <untakenprefix@gmail.com>
2020-07-27 23:45:29 +1000

    `ChatBar` fills sent messages on uparrow/downarrow
    Will later consider tab username autocomplete.

--- BelieveAndRise/UI/ViewController/ChatViewController/ChatBar.swift
+++ BelieveAndRise/UI/ViewController/ChatViewController/ChatBar.swift
@@ -12,11 +12,18 @@ protocol ChatBarDelegate: AnyObject {
     func chatBar(_ chatBar: ChatBar, shouldSendMessage message: String) -> Bool
 }
 
-final class ChatBar: NSView, NibLoadable {
+/// Wraps an NSTextField with capabilities associated with sending messages.
+final class ChatBar: NSView, NibLoadable, NSTextFieldDelegate, NSControlTextEditingDelegate {
 
     // MARK: - Dependencies
 
     weak var delegate: ChatBarDelegate?
+    private var sentMessages: [String] = []
+    private var messageCache: String = ""
+    private var historyIndex: Int? = nil
+    private var lookingAtHistory: Bool {
+        historyIndex != nil
+    }
 
     // MARK: - Lifecycle
 
@@ -26,7 +33,11 @@ final class ChatBar: NSView, NibLoadable {
 
     // MARK: - UI Components
 
-    @IBOutlet var textField: NSTextField!
+    @IBOutlet var textField: NSTextField! {
+        didSet {
+            textField?.delegate = self
+        }
+    }
     @IBOutlet var button: NSButton!
 
     @IBOutlet var bottomConstraint: NSLayoutConstraint!
@@ -61,10 +72,52 @@ final class ChatBar: NSView, NibLoadable {
     // MARK: - Actions
 
     @IBAction func sendMessage(_ sender: Any) {
+        let message = textField.stringValue
 
         if let delegate = delegate,
-            delegate.chatBar(self, shouldSendMessage: textField.stringValue) {
+            delegate.chatBar(self, shouldSendMessage: message) {
+            if sentMessages.first != message {
+                sentMessages.insert(message, at: 0)
+            }
+            historyIndex = nil
             textField.stringValue = ""
         }
+    }
+
+    // MARK: - NSTextFieldDelegate
+
+    func control(_ control: NSControl, textView: NSTextView, doCommandBy commandSelector: Selector) -> Bool {
+        switch commandSelector {
+        case #selector(moveUp(_:)):
+            let newHistoryIndex: Int
+            if let historyIndex = historyIndex {
+                newHistoryIndex = historyIndex + 1
+            } else {
+                messageCache = textField.stringValue
+                newHistoryIndex = 0
+            }
+            if newHistoryIndex < sentMessages.count {
+                self.historyIndex = newHistoryIndex
+                textField.stringValue = sentMessages[newHistoryIndex]
+            }
+        case #selector(moveDown(_:)):
+            if let historyIndex = historyIndex {
+                if historyIndex == 0 {
+                    self.historyIndex = nil
+                    textField.stringValue = messageCache
+                } else {
+                    let newHistoryIndex = historyIndex - 1
+                    self.historyIndex = newHistoryIndex
+                    textField.stringValue = sentMessages[newHistoryIndex]
+                }
+            }
+        default:
+            return false
+        }
+        return true
+    }
+
+    func controlTextDidChange(_ obj: Notification) {
+        historyIndex = nil
     }
 }
